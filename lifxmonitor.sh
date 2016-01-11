a6e841e4687aa1985a650227cf96ec0f88e1acec
Thomas Brandl <thobra@gmail.com>
2016-01-11 20:48:40 +0100

    Added forever script

---
+++ lifxmonitor.sh
@@ -0,0 +1,27 @@
+### BEGIN INIT INFO
+# Provides:             LifxMonitor
+# Required-Start:
+# Required-Stop:
+# Default-Start:        2 3 4 5
+# Default-Stop:         0 1 6
+# Short-Description:    RFDuino Sensor Node App
+### END INIT INFO
+
+export NODE_PATH=$NODE_PATH:/usr/local/bin
+export HOME=/root
+NODESCRIPT=/home/pi/LifxMonitor/
+
+case "$1" in
+  start)
+    sudo -u pi forever start -p /home/pi/.forever --sourceDir=$NODESCRIPT index.js
+    ;;
+  stop)
+    sudo -u pi forever stop -p /home/pi/.forever ${NODESCRIPT}index.js
+    ;;
+  *)
+
+  echo "Usage: /etc/init.d/LifxMonitor {start|stop}"
+  exit 1
+  ;;
+esac
+exit 0
